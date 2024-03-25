00a814122fd5034bfcddf16c10da42414af13c79
ikhsaneff <88372794+ikhsaneff@users.noreply.github.com>
2024-03-25 17:48:18 +0700

    Add functions example code

---
+++ Functions/average.c
@@ -0,0 +1,18 @@
+/* Computes pairwise averages of three numbers */
+#include <stdio.h>
+
+double averagen(double a[], int n)
+{
+    double sum=0.0; int i;
+    for (i=0; i<n; i++) sum += a[i];
+    return sum/n;
+}
+
+int main(void)
+{
+    double x[3];
+    printf("Enter three numbers: ");
+    scanf("%lf%lf%lf", &x[0], &x[1], &x[2]);
+    printf("Average: %f\n", average(x, 3));
+    return 0;
+}
