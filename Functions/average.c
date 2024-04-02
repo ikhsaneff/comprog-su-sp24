ea551a996032f38a33d9a5e808b2536a482cb935
ikhsaneff <88372794+ikhsaneff@users.noreply.github.com>
2024-04-02 21:21:40 +0700

    add max_min

--- Functions/average.c
+++ Functions/average.c
@@ -13,6 +13,6 @@ int main(void)
     double x[3];
     printf("Enter three numbers: ");
     scanf("%lf%lf%lf", &x[0], &x[1], &x[2]);
-    printf("Average: %f\n", average(x, 3));
+    printf("Average: %f\n", averagen(x, 3));
     return 0;
 }
